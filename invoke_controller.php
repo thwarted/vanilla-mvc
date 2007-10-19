e1f3182ba12e5e91277495ccf24b5757f7a5314a
thwarted <github@thwartedefforts.org>
2007-10-19 00:44:23 +0000

    removed mk_invoke_link, useless, and replaced with lib.php's url() function
    added invoke_form_handler, will try to find a handle_form_X function for form data in subarrays named model-X

--- invoke_controller.php
+++ invoke_controller.php
@@ -1,5 +1,59 @@
 <?php
 
+
+function invoke_controller($request) {
+
+    $controller = array_shift($request);
+    if (preg_match('/\W/', $controller)) {
+        throw new HTTPNotFound('illegal characters in controller name (1)');
+    }
+    $controller_file = "controllers/$controller.php";
+    if ($controller === '_media') {
+        $controller_file = "vanilla/dynamic_media.php";
+        $controller_class = "dynamic_media_controller";
+    } else {
+        if (preg_match('/^_/', $controller)) {
+            throw new HTTPNotFound('illegal characters in controller name (2)');
+        }
+        $controller_class = "controller_$controller";
+    
+        if (!file_exists($controller_file)) {
+            throw new HTTPNotFound("controller $controller: file not found");
+        }
+    }
+
+    require_once($controller_file);
+
+    if (!class_exists($controller_class)) {
+        throw new HTTPNotFound("controller $controller: code not found");
+    }
+
+    $controller = new $controller_class($request);
+    return $controller;
+
+}
+
+function invoke_form_handler() {
+    foreach ($_POST as $k=>$a) {
+        if (is_array($a)) {
+            # - is illegal in function names in PHP, so safe to use as a separator here
+            list($model, $formid) = split('-', $k);
+            if ($formid) {
+                $modelfile = sprintf('models/%s.php', $model);
+                $handlefunc = sprintf('handle_form_%s', $formid);
+                if (file_exists($modelfile)) {
+                    require_once $modelfile;
+                    if (method_exists($model, $handlefunc)) {
+                        call_user_func(array($model, $handlefunc));
+                    }
+                } 
+            } # no - in form name
+        } # not array
+    }
+}
+
+
+/*
 function mk_invoke_link() {
     $a = func_get_args();
     if (count($a) == 1 && is_array($a[0])) {
@@ -54,37 +108,4 @@ function mk_invoke_link() {
 
     return $b;
 }
-
-function invoke_controller($request) {
-
-    $controller = array_shift($request);
-    if (preg_match('/\W/', $controller)) {
-        throw new HTTPNotFound('illegal characters in controller name (1)');
-    }
-    $controller_file = "controllers/$controller.php";
-    if ($controller === '_media') {
-        $controller_file = "vanilla/dynamic_media.php";
-        $controller_class = "dynamic_media_controller";
-    } else {
-        if (preg_match('/^_/', $controller)) {
-            throw new HTTPNotFound('illegal characters in controller name (2)');
-        }
-        $controller_class = "controller_$controller";
-    
-        if (!file_exists($controller_file)) {
-            throw new HTTPNotFound("controller $controller: file not found");
-        }
-    }
-
-    require_once($controller_file);
-
-    if (!class_exists($controller_class)) {
-        throw new HTTPNotFound("controller $controller: code not found");
-    }
-
-    $controller = new $controller_class($request);
-    return $controller;
-
-}
-
-
+*/
